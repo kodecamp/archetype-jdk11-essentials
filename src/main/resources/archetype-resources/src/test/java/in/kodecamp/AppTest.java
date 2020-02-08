e5564a07f6943747696f15fb655c837d29315538
kodecamp <kodecamp.in@gmail.com>
2020-02-08 11:36:31 +0530

    initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/in/kodecamp/AppTest.java
@@ -0,0 +1,21 @@
+package in.kodecamp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+  /**
+   * @return the suite of tests being tested
+   */
+
+  @Test
+  public void testApp() {
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+    assertEquals("Sunil", "Sunil", "Not Equal");
+  }
+}
